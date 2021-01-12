907d0b69d2d2a627b63fcbe87c8fc398cf7b3849
Orsiris de Jong <ozy@netpower.fr>
2021-01-12 11:56:08 +0100

    Enable cryptidy <1.0.0 compatibility shim

--- cryptidy/symmetric_encryption.py
+++ cryptidy/symmetric_encryption.py
@@ -119,15 +119,14 @@ def decrypt_message(msg, aes_key):
     Simple base64 wrapper for aes_decrypt_message
     """
     verify_key(aes_key)
-    # try:  # COMPAT-0.9
-    try:
-        decoded_msg = b64decode(msg)
-    except (TypeError, binascii_Error):
-        raise TypeError('decrypt_message accepts b64 encoded byte objects')
-
-    return aes_decrypt_message(decoded_msg, aes_key)
-    # except:  # COMPAT-0.9
-    #    return aes_decrypt_message(msg, aes_key)  # COMPAT-0.9
+    try:  # COMPAT-0.9
+        try:
+            decoded_msg = b64decode(msg)
+        except (TypeError, binascii_Error):
+            raise TypeError('decrypt_message accepts b64 encoded byte objects')
+        return aes_decrypt_message(decoded_msg, aes_key)
+    except Exception:  # pylint: disable=W0703,broad-except  # COMPAT-0.9
+        return aes_decrypt_message(msg, aes_key)  # COMPAT-0.9
 
 
 def aes_decrypt_message(msg, aes_key):
@@ -144,19 +143,19 @@ def aes_decrypt_message(msg, aes_key):
                                          msg[32:64],
                                          msg[64:])
 
-    # try:  # COMPAT-0.9
-    source_timestamp = float(unpad(timestamp.decode('utf-8')))
-    # Python < 3.4 does not have timestamp() attribute
-    if sys.version_info[0] < 3 or sys.version_info[1] < 4:
-        timestamp_now = time.mktime(datetime.utcnow().timetuple())
-    else:
-        timestamp_now = datetime.utcnow().timestamp()
-    if source_timestamp > timestamp_now:
-        raise ValueError('Timestamp is in future')
-    source_timestamp = datetime.fromtimestamp(source_timestamp)
-    # except:  # COMPAT-0.9
-    #    source_timestamp = None  # COMPAT-0.9
-    #    ciphertext = msg[32:]  # COMPAT-0.9
+    try:  # COMPAT-0.9
+        source_timestamp = float(unpad(timestamp.decode('utf-8')))
+        # Python < 3.4 does not have timestamp() attribute
+        if sys.version_info[0] < 3 or sys.version_info[1] < 4:
+            timestamp_now = time.mktime(datetime.utcnow().timetuple())
+        else:
+            timestamp_now = datetime.utcnow().timestamp()
+        if source_timestamp > timestamp_now:
+            raise ValueError('Timestamp is in future')
+        source_timestamp = datetime.fromtimestamp(source_timestamp)
+    except Exception:  # pylint: disable=W0703,broad-except # COMPAT-0.9
+        source_timestamp = None  # COMPAT-0.9
+        ciphertext = msg[32:]  # COMPAT-0.9
 
     try:
         data = aes_decrypt(aes_key, nonce, tag, ciphertext)
