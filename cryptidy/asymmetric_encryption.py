b38d426c1744d4b08a51ee34f6788b14712fcd14
Florex <108609121+tetie12345@users.noreply.github.com>
2024-12-31 20:44:07 +0100

    Substituted ignore_warnings for error_handling

--- cryptidy/asymmetric_encryption.py
+++ cryptidy/asymmetric_encryption.py
@@ -170,19 +170,14 @@ def decrypt_message_hf(
     )
 
 
-def decrypt_message(msg, private_key, error_handling=3):
+def decrypt_message(msg, private_key, ignore_warnings=False):
     # type: (Union[bytes, str], str) -> Tuple[datetime, Any]
     """
     Simple base64 wrapper for rsa_decrypt_message
 
-    error_handling is the level of error handling:
-    3 means all errors and warnings are handled
-    2 means only errors are handled and warnings are ignored
-    1 means everything is ignored (this may cause some things to break, only use if absolutely necessary)
-
     :param msg: b64 encoded original rsa encrypted data
     :param private_key: rsa private key
-    :param error_handling: level of error handling
+    :param ignore_warnings: whether to ignore warnings
     :return: (bytes): rsa decrypted data
     """
     verify_key(private_key, "RSA PRIVATE")
@@ -193,22 +188,17 @@ def decrypt_message(msg, private_key, error_handling=3):
             "decrypt_message accepts b64 encoded byte objects"
         )  # pylint: disable=W0707,raise-missing-from
 
-    return rsa_decrypt_message(decoded_msg, private_key, error_handling)
+    return rsa_decrypt_message(decoded_msg, private_key, ignore_warnings)
 
 
-def rsa_decrypt_message(msg, private_key, error_handling=3):
+def rsa_decrypt_message(msg, private_key, ignore_warning=False):
     # type: (bytes, str) -> Tuple[datetime, Any]
     """
     RSA decrypt a python object / bytes / string and check the encryption timestamp
 
-    error_handling is the level of error handling:
-    3 means all errors and warnings are handled
-    2 means only errors are handled and warnings are ignored
-    1 means everything is ignored (this may cause some things to break, only use if absolutely necessary)
-
     :param msg: original rsa encrypted data
     :param private_key: rsa encryption key
-    :param error_handling: level of error handling
+    :param ignore_warnings: whether to ignore warnings
     :return: original data
     """
     private_key = RSA.import_key(private_key)
@@ -238,4 +228,4 @@ def rsa_decrypt_message(msg, private_key, error_handling=3):
         else:
             exec(err + " from None")
 
-    return aes_decrypt_message(aes_encrypted_msg, session_key, error_handling)
+    return aes_decrypt_message(aes_encrypted_msg, session_key, ignore_warnings)
